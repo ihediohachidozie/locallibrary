25da162ad17a36229a3f2153134f1bd7ae78fe28
Ihedioha Chidozie <Ihediohachidozie@gmail.com>
2024-03-22 14:33:32 -0400

    Created locallibrary models

--- main.py
+++ main.py
@@ -6,6 +6,14 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
+    LOAN_STATUS = (
+        ('m', 'Maintenance'),
+        ('o', 'On loan'),
+        ('a', 'Available'),
+        ('r', 'Reserved'),
+    )
+    print(LOAN_STATUS[0][1])
+
     print(f"Hi, {name}")  # Press Ctrl+F8 to toggle the breakpoint.
 
 
