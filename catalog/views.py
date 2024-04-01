18141756ccb947b900c6e74e720ed614aa4f6c4d
Ihedioha Chidozie <Ihediohachidozie@gmail.com>
2024-04-01 12:15:58 -0400

    session framework

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,71 @@
 from django.shortcuts import render
+from .models import Book, Author, BookInstance, Genre
+from django.views import generic
+
 
 # Create your views here.
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    # Challenge
+    num_genres = Genre.objects.all().count()
+
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_books_available = Book.objects.filter(title__icontains='a').count()
+
+    # The 'all()' is implied by default.
+    num_authors = Author.objects.count()
+
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_genres': num_genres,
+        'num_books_available': num_books_available,
+        'num_visits': num_visits,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
+
+
+class BookListView(generic.ListView):
+    model = Book
+    context_object_name = 'book_list'  # your own name for the list as a template variable
+
+    def get_queryset(self):
+        return Book.objects.all()  # Get ALl books
+
+    # queryset = Book.objects.filter(title__incontains='war')[:5]  # Get 5 books containing the title war
+    template_name = 'book_list.html'  # Specify your own template name/location
+    paginate_by = 2
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+    template_name = 'book_detail.html'
+
+
+class AuthorListView(generic.ListView):
+    model = Author
+    context_object_name = 'author_list'  # your own name for the list as a template variable
+
+    def get_queryset(self):
+        return Author.objects.all()  # Get all author
+
+    template_name = 'author_list.html'  # Specify the template name
+    paginate_by = 2
+
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+    template_name = 'author_detail.html'
